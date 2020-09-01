b3dc7f5efe11590e14e49d8083681d2d302577b0
John Edwards <jdedwards6789@gmail.com>
2020-09-01 09:58:44 -0400

    Intermediate progress on refactoring code into separate files

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const cTable = require("console.table");
+const add = require("./lib/add.js");
+const del = require("./lib/delete");
+const update = require("./lib/update");
+const view = require("./lib/view");
 
 //local versions of DB tables
 let department = [];
@@ -158,7 +162,6 @@ function updateLocalEmployee() {
 
 /////////////////////////////////////////////////////////////
 // Prompt User for Input
-
 /////////////////////////////////////////////////////////////
 function promptUser() { // prompts the user for an action: add, delete, update, view, exit 
     inquirer.prompt({
@@ -186,17 +189,20 @@ function promptUser() { // prompts the user for an action: add, delete, update, 
             new inquirer.Separator("----EXIT---"),
             "Exit"
         ]
-    }).then(function (response) {
+    }).then(async function (response) {
         switch (response.action) {// determine user action, call appropriate function
             //ADD
             case "Add Department":
-                addDepartment();
+                add.addDepartment(connection);
+                updateLocalDepartment();
                 break;
             case "Add Role":
-                addRole();
+                add.addRole(connection);
+                updateLocalRole();
                 break;
             case "Add Employee":
-                addEmployee();
+                addEmployee(connection);
+                updateLocalEmployee();
                 break;
 
             //DELETE
@@ -244,146 +250,7 @@ function promptUser() { // prompts the user for an action: add, delete, update, 
     });
 }
 
-/////////////////////////////////////////////////////////////
-// ADD DEPARTMENT, ROLE, OR EMPLOYEE
-/////////////////////////////////////////////////////////////
-function addDepartment() {// asks user for new department name, then adds department to database, and shows departments
-    inquirer.prompt({
-        name: "department",
-        type: "input",
-        message: "Input new department name, or X to cancel:",
-    }).then(function (response) {
-        if (response.department == "X") { // if user wants to escape, call promptUser()
-            promptUser();
-        } else {
-            connection.query(
-                `INSERT INTO department (name)
-                VALUES (?);`,
-                [response.department], //user response
-                async function (err) { // async anonymous function to await updateLocalDepartment() promise
-                    if (err) throw err;
-                    await updateLocalDepartment(); // update the local department tables
-                    viewDepartments(); //viewDepartments will call promptUser() after displaying departments
-                });
-        }
-    });
-}
 
-function addRole() {
-    inquirer.prompt([ // prompt user for role title, role salary, and to select an existing department
-        {
-            name: "title",
-            type: "input",
-            message: "Input new role title, or X to cancel:",
-        },
-        {
-            name: "salary",
-            type: "input",
-            message: "Input new role salary, or 0 to cancel:",
-            validate: async function (input) {
-                if (isNaN(input)){//typeof input !== "number") {
-                    return "Please enter a number";
-                }
-                return true;
-            },
-        },
-        {
-            name: "department",
-            type: "list",
-            message: "Select a department for this role:",
-            choices: function () { // creates a array of department names for users to choose from
-                let choiceArray = [];
-                for (let i = 0; i < department.length; i++) {
-                    choiceArray.push(`${department[i].name}`);
-                }
-                choiceArray.push("Cancel");
-                return choiceArray;
-            }
-        }
-    ]).then(function (response) { // response is user response
-        if (response.name == "X" || response.salary == 0 || response.department == "Cancel") { // if user wants to escape, call promptUser()
-            console.log("Returning to Main Menu");
-            promptUser();
-        } else {
-            let selectedDepartment = department.find(item => item.name === response.department) // find where user response === the name of a department
-            connection.query( // insert new role in role table
-                `INSERT INTO role (title, salary, department_id)
-                            VALUES (?, ?, ?);`,
-                [response.title, response.salary, selectedDepartment.id], //user response, use the id of the selectedDepartment as the foreign key in the role table
-                async function (err) { // async anonymous function to await updateLocalRole() promise
-                    if (err) throw err;
-                    await updateLocalRole(); // update the local role table from the database
-                    viewRoles(); // view roles will call promptUser() after displaying roles
-                });
-        }
-    });
-}
-
-function addEmployee() {
-    inquirer.prompt([ // prompt for new employee first name, last name, role from existing roles, manager from existing employees
-        {
-            name: "first",
-            type: "input",
-            message: "Enter Employee First Name",
-        },
-        {
-            name: "last",
-            type: "input",
-            message: "Enter Employee Last Name",
-        },
-        {
-            name: "role",
-            type: "list",
-            message: "Select a role for this employee:",
-            choices: function () {
-                let choiceArray = [];
-                for (let i = 0; i < role.length; i++) {
-                    choiceArray.push(`${role[i].title}`);
-                }
-                choiceArray.push("Cancel");
-                return choiceArray;
-            }
-        },
-        {
-            name: "manager",
-            type: "list",
-            message: "Select a manager for this employee (or null/blank):",
-            choices: function () {
-                let choiceArray = ["null"]; // first choice is null
-                for (let i = 0; i < employee.length; i++) {
-                    choiceArray.push(`${employee[i].first_name} ${employee[i].last_name}`);
-                }
-                return choiceArray;
-            }
-        }
-    ]).then(function (response) {
-        if (response.first == "X" || response.last == "X" || response.role == "Cancel") { // if user wants to escape, call promptUser()
-            console.log("Returning to Main Menu");
-            promptUser();
-        } else {
-            let selectedRole = role.find(item => item.title === response.role) // role object where user role object title === user response.role
-            let selectedManagerID = null; // default to no manager for new employee
-            if (response.manager != "null") { // if user response for manager was not "null"
-                let managerName = response.manager.split(" "); // split response manager name into "first_name" and "last_name"
-                selectedManager = employee.find(item => { // find employee id of employee who's name matches the user input
-                    if ((item.first_name === managerName[0]) && (item.last_name === managerName[1])) {
-                        console.log(`${managerName} = ${item.first_name} + ${item.last_name}`);
-                        return true;
-                    }
-                });
-            }
-            connection.query(
-                `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-                            VALUES (?, ?, ?, ?);`,
-                [response.first.trim(), response.last.trim(), selectedRole.id, selectedManager.id],//selectedManagerID], //user response
-                async function (err) { // async anonymous function to await updating local tables                  
-                    if (err) throw err;
-                    await updateLocalEmployee(); // updateLocalEmployee() will update local employee tables
-                    viewEmployees(); //viewDepartments will call promptUser() after displaying departments
-                });
-        }
-    });
-}
 
 /////////////////////////////////////////////////////////////
 // DELETE DEPARTMENT, ROLE, OR EMPLOYEE
